276faebcfaad5db7845b737e22dadbbd96848b93
botolkaca23 <botolkaca23@gmail,com>
2023-07-09 23:12:24 +0700

    final projek

---
+++ app/Filament/Resources/AdminResource/Pages/Settings.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\AdminResource\Pages;
+
+use App\Filament\Resources\AdminResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class Settings extends ManageRecords
+{
+    protected static string $resource = AdminResource::class;
+}
---
+++ app/Filament/Resources/PenggunaResource/Pages/Settings.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\PenggunaResource\Pages;
+
+use App\Filament\Resources\PenggunaResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class Settings extends ViewRecord
+{
+    protected static string $resource = PenggunaResource::class;
+}
